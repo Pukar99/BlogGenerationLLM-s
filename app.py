9d5675abb6f98e1582aae0f42dfca67e95997b40
Pukar Sharma <sharma.pukar99@gmail.com>
2024-04-01 13:41:32 +0545

    completed

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 
 ## Function To get response from LLAma 2 model
-
-def getLLamaresponse(input_text,no_words,blog_style):
-
+def getLLamaresponse(input_text, no_words, blog_style):
     ### LLama2 model
     llm=CTransformers(model='model/llama-2-7b-chat.ggmlv3.q8_0.bin',
                       model_type='llama',
@@ -13,46 +11,59 @@ def getLLamaresponse(input_text,no_words,blog_style):
                               'temperature':0.01})
     
     ## Prompt Template
-
-    template="""
-        Write a blog for {blog_style} job profile for a topic {input_text}
-        within {no_words} words.
-            """
+    template = """
+    Write a blog for {blog_style} job profile on the topic of "{input_text}" within {no_words} words.
+    """
     
-    prompt=PromptTemplate(input_variables=["blog_style","input_text",'no_words'],
+    prompt = PromptTemplate(input_variables=["blog_style","input_text",'no_words'],
                           template=template)
     
-    ## Generate the ressponse from the LLama 2 model
-    response=llm(prompt.format(blog_style=blog_style,input_text=input_text,no_words=no_words))
-    print(response)
+    ## Generate the response from the LLama 2 model
+    response = llm(prompt.format(blog_style=blog_style, input_text=input_text, no_words=no_words))
     return response
 
-
-
-
-
-
-st.set_page_config(page_title="Generate Blogs",
+def main():
+    st.set_page_config(page_title="Generate Blogs",
                     page_icon='🤖',
                     layout='centered',
                     initial_sidebar_state='collapsed')
 
-st.header("Generate Blogs 🤖")
+    st.header("Generate Blogs 🤖")
 
-input_text=st.text_input("Enter the Blog Topic")
+    input_text = st.text_input("Enter the Blog Topic")
 
-## creating to more columns for additonal 2 fields
+    ## creating two more columns for additional 2 fields
+    col1, col2 = st.columns([5,5])
 
-col1,col2=st.columns([5,5])
+    with col1:
+        no_words = st.text_input('No of Words')
 
-with col1:
-    no_words=st.text_input('No of Words')
-with col2:
-    blog_style=st.selectbox('Writing the blog for',
-                            ('Researchers','Data Scientist','Common People'),index=0)
+    with col2:
+        blog_style = st.selectbox('Writing the blog for',
+                                ('Researchers', 'Data Scientist', 'Common People'), index=0)
     
-submit=st.button("Generate")
+    submit = st.button("Generate")
 
-## Final response
-if submit:
-    st.write(getLLamaresponse(input_text,no_words,blog_style))
+    ## Final response
+    if submit:
+        generated_blog = getLLamaresponse(input_text, no_words, blog_style)
+        
+        st.subheader("Generated Blog:")
+        st.write(generated_blog)
+
+        # Enhancements:
+        # 1. Allow user interaction to modify the generated blog
+        st.subheader("Modify Blog:")
+        modified_blog = st.text_area("Edit the Generated Blog", value=generated_blog, height=200)
+
+        # 2. Add social sharing buttons
+        st.subheader("Share Blog:")
+        if st.button("Share on Twitter"):
+            # Code to share the blog on Twitter
+            pass
+        if st.button("Share on Facebook"):
+            # Code to share the blog on Facebook
+            pass
+
+if __name__ == "__main__":
+    main()
